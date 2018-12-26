3e3c713e7e2cd4d4dc34358df8331b3f2556bb46
Sema <sema@pyva.net>
2018-12-25 23:13:44 -0500

    Song structure

--- javascript/JZZ.gui.Karaoke.js
+++ javascript/JZZ.gui.Karaoke.js
@@ -16,81 +16,105 @@
   function Karaoke(at) {
     var self = new JZZ.Widget();
     for (var k in Karaoke.prototype) if (Karaoke.prototype.hasOwnProperty(k)) self[k] = Karaoke.prototype[k];
-    self.spans = [];
-    self.current = 0;
-    self.div = document.createElement('div');
-    self.div.className = 'karaoke';
+    self.tracks = [];
+    try {
+      self.gui = document.createElement('div');
+    }
+    catch (e) {
+      return self; // not in browser
+    }
+    self.gui.className = 'karaoke';
     if (typeof at == 'string') {
       try {
-        document.getElementById(at).appendChild(self.div);
+        document.getElementById(at).appendChild(self.gui);
         return self;
       }
       catch(e) {}
     }
     try {
-      at.appendChild(self.div);
+      at.appendChild(self.gui);
       return self;
     }
     catch(e) {}
-    document.body.appendChild(self.div);
+    document.body.appendChild(self.gui);
     return self;
   }
   Karaoke.prototype.load = function(smf) {
-    var i, j, msg, txt, cl;
-    var par, div, span;
-    while (this.div.firstChild) {
-      this.div.removeChild(this.div.firstChild);
+    var i, j, msg, txt, cl, tt;
+    var track, verse, line, div, span;
+    if (this.gui) while (this.gui.firstChild) {
+      this.gui.removeChild(this.gui.firstChild);
     }
-    this.spans = [];
-    this.current = 0;
+    this.tracks = [];
     for (i = 0; i < smf.length; i++) {
       if (smf[i] instanceof JZZ.MIDI.SMF.MTrk) {
-        par = undefined;
+        track = undefined;
         for (j = 0; j < smf[i].length; j++) {
           if (smf[i][j].ff == 1) {
+            tt = smf[i][j].tt;
+            if (!track) {
+              track = { tt: tt, verses: [] };
+              if (this.gui) {
+                track.dom = document.createElement('div');
+                this.gui.appendChild(track.dom);
+              }
+              this.tracks.push(track);
+              verse = undefined;
+            }
             txt = JZZ.lib.fromUTF8(smf[i][j].dd);
             if (txt[0] == '@') {
-              div = document.createElement('div');
               cl = { K: 'k', V: 'v', I: 'i', L: 'l', T: 't', W: 'w' }[txt[1]];
-              if (cl) {
-                txt = txt.substring(2);
-                div.className = cl;
+              if (cl) txt = txt.substring(2);
+              if (this.gui) {
+                div = document.createElement('div');
+                if (cl) div.className = cl;
+                div.appendChild(document.createTextNode(txt));
+                track.dom.appendChild(div);
               }
-              div.appendChild(document.createTextNode(txt));
-              this.div.appendChild(div);
-              par = undefined;
+              if (cl == 't') track.title = track.title ? track.title + '\n' + txt : txt;
+              verse = undefined;
               continue;
             }
             if (txt[0] == '\\') {
               txt = txt.substring(1);
-              par = undefined;
+              verse = undefined;
             }
             else if (txt[0] == '/') {
               txt = txt.substring(1);
-              div = undefined;
+              line = undefined;
             }
-            if (!par) {
-              par = document.createElement('p');
-              this.div.appendChild(par);
-              div = undefined;
+            if (!verse) {
+              verse = { tt: tt, lines: [] };
+              if (this.gui) {
+                verse.dom = document.createElement('p');
+                track.dom.appendChild(verse.dom);
+              }
+              track.verses.push(verse);
+              line = undefined;
             }
-            if (!div) {
-              div = document.createElement('div');
-              par.appendChild(div);
+            if (!line) {
+              line = { tt: tt, spans: [] };
+              if (this.gui) {
+                line.dom = document.createElement('div');
+                verse.dom.appendChild(line.dom);
+              }
+              verse.lines.push(line);
             }
-            span = document.createElement('span');
-            span.appendChild(document.createTextNode(txt));
-            div.appendChild(span);
-            this.spans.push([smf[i][j].tt, span]);
+            span = { tt: tt, txt: txt };
+            if (this.gui) {
+              span.dom = document.createElement('span');
+              span.dom.appendChild(document.createTextNode(txt));
+              line.dom.appendChild(span.dom);
+            }
+            line.spans.push(span);
           }
         }
       }
     }
-    this.spans.sort(function(a, b) { return a[0] - b[0]; });
   };
   Karaoke.prototype.reset = function() {
-    for (var i = 0; i < this.spans.length; i++) this.spans[i][1].className = '';
-    this.current = 0;
+    //for (var i = 0; i < this.spans.length; i++) this.spans[i][1].className = '';
+    //this.current = 0;
   };
   Karaoke.prototype._receive = function(msg) {
     if (typeof msg.tt != 'undefined') {
