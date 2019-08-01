e8ea14e592f3c72eea2d4b965fa7fc2e09af07eb
Sema <sema@pyva.net>
2019-07-31 23:54:57 -0400

    Improved coverage

---
+++ test/mocha2.js
@@ -0,0 +1,24 @@
+var JSDOM = require('jsdom').JSDOM;
+var assert = require('assert');
+
+global.__coverage__ = {};
+
+describe('In browser', function() {
+  before(function() {
+    return new Promise(function(resolve) {
+      JSDOM.fromFile('test/test.html', {
+        resources: 'usable',
+        runScripts: 'dangerously',
+        beforeParse: function(window) {
+          window.__coverage__ = global.__coverage__;
+        }
+      }).then(function(dom) {
+        global.window = dom.window;
+        setTimeout(resolve, 1500);
+      });
+    });
+  });
+  it('it works!', function() {
+    assert.equal(0, 0);
+  });
+});
