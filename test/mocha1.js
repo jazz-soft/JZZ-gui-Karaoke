eed90864cefefd680ec6f727f612b82735b141b8
Sema <sema@pyva.net>
2019-07-31 23:24:47 -0400

    Missing test folder

---
+++ test/mocha1.js
@@ -0,0 +1,20 @@
+var JZZ = require('jzz');
+require('jzz-midi-smf')(JZZ);
+require('..')(JZZ);
+var assert = require('assert');
+
+describe('In terminal', function() {
+  it('it works!', function(done) {
+    assert.equal(0, 0);
+    var file = 'mary.mid';
+    var data = require('fs').readFileSync(file, 'binary');
+    var smf = new JZZ.MIDI.SMF(data);
+    var kar = new JZZ.gui.Karaoke();
+    kar.load(smf);
+    var player = smf.player();
+    player.connect(kar);
+    player.speed(20);
+    player.play();
+    setTimeout(done, 2000);
+  }).timeout(2500);
+});
